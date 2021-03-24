a5f845eb0bc02eb748943730a556339d05bb0bdd
lucasfreitas242 <lucasfrittas@gmail.com>
2021-03-24 02:07:21 -0300

    Criação do Controller Sessions para Autenticação de Usuários

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  
 
   resources :users, only:[:new, :create]
 
