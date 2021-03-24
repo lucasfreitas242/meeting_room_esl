6354e9b261b251cf4442561b1f5e232323496de3
lucasfreitas242 <lucasfrittas@gmail.com>
2021-03-24 01:58:35 -0300

    Internacionalização das mensagens de erro no cadastro de usuário

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module MeetingRoom
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :'pt-BR'
+
+    
   end
 end
