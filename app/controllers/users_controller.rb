e631cbe76bfcb3c116a7ee8a00decec0c93df1f6
Lucas Freitas <lucasfrittas@gmail.com>
2021-03-24 01:39:45 -0300

    Finalização do envio POST para Users. Criação da Partial 'error_messages'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = 'Usuário cadastrado com sucesso!'
+            redirect_to root_url
+        else  
+            render 'new'
+        end
+        
     end
+
+    private
+        def user_params
+            params.require(:user).permit(:email, :name, :password, :password_confirmation)
+        end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i 
 
-    validates :nome, presence: true, length: {maximum: 50}
+    validates :name, presence: true, length: {maximum: 50}
     validates :password, length: {minimum: 6}
     validates :email, presence: true, length: { maximum: 255 },
                                       format: { with: VALID_EMAIL_REGEX },
