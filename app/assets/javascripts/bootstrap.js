e59b8a608f9f4be598902806f50c3e71ceb1b041
lucasfreitas242 <lucasfrittas@gmail.com>
2021-03-26 01:52:36 -0300

    Testes referentes ao CSS na GEM simple_calendars

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,4 @@
 jQuery(function() {
-  $("a[rel~=popover], .has-popover").popover();
-  $("a[rel~=tooltip], .has-tooltip").tooltip();
-});
+    $("a[rel~=popover], .has-popover").popover();
+    $("a[rel~=tooltip], .has-tooltip").tooltip();
+  });
